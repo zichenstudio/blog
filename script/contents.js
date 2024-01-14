8cc847ba0a45bc1d4c927755048b6421d9bbfc3e
zichenstudio@outlook.com <zichenstudio@outlook.com>
2024-01-15 00:13:50 +0800

    Update contents

--- script/contents.js
+++ script/contents.js
@@ -1,17 +1,36 @@
-(function () {
-    const content = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
-    let result = "";
-    for (let i = 1; i < content.length; i++) {
-        result += `<a href="#${content[i].id}" onclick="smoothScroll(event)">
-            ${content[i].tagName
-                ?.replace("H1", "# ")
-                ?.replace("H2", "## ")
-                ?.replace("H3", "### ")
-                ?.replace("H4", "#### ")
-                ?.replace("H5", "##### ")
-                ?.replace("H6", "###### ")}
-            ${content[i].innerText}
-            <br>`;
-    }
-    document.getElementById("content").innerHTML = result;
-}())
+(function() {
+    let headings = document.querySelectorAll('h1:not(:first-child), h2, h3, h4, h5, h6');
+
+    let contentDiv = document.createElement('div');
+    contentDiv.id = 'content';
+
+    let currentParent = contentDiv;
+    let previousLevel = 1;
+
+    headings.forEach(function(heading, index) {
+        let level = parseInt(heading.tagName.charAt(1));
+        let wrapper = document.createElement('div');
+        let anchor = document.createElement('a');
+        anchor.innerHTML = heading.innerHTML;
+        anchor.href = `#${heading.id}`;
+        anchor.onclick = smoothScroll;
+        wrapper.appendChild(anchor);
+
+        if (level > previousLevel) {
+            currentParent.appendChild(wrapper);
+            currentParent = wrapper;
+        } else if (level < previousLevel) {
+            for (let i = level; i < previousLevel; i++) {
+                currentParent = currentParent.parentNode;
+            }
+            currentParent.parentNode.appendChild(wrapper);
+            currentParent = wrapper;
+        } else {
+            contentDiv.appendChild(wrapper);
+        }
+
+        previousLevel = level;
+    });
+
+    document.body.insertBefore(contentDiv, document.body.firstChild);
+})()
